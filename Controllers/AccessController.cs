9a04a83c3ecb7b35d171c6680a59ef16f682c4df
Ed. Gi <31946346+dwaxgio@users.noreply.github.com>
2020-10-11 21:01:38 -0500

    Se agregó AccessViewModel con propiedades de email y contraseña, se creó en AccessController un método Login que recibe como parametro el modelo. se consulta email y contraseña, se genera GUID a oReply.data, y se guarda token a usuario consultado para luego enviar por cuerpo json en postman los datos de validación, se guarda token en db

--- Controllers/AccessController.cs
+++ Controllers/AccessController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using MVC_API_FUNDAMENTALS.Models.WS;
+using MVC_API_FUNDAMENTALS.Models;
 
 namespace MVC_API_FUNDAMENTALS.Controllers
 {
@@ -21,5 +22,48 @@ namespace MVC_API_FUNDAMENTALS.Controllers
 
             return oReply;
         }
+
+        // 5. Se agrega nuevo metodo, luego crear la db, tabla y el ef, para autentificación por medio de post
+        [HttpPost]
+        public Reply Login([FromBody] AccessViewModel model) // Se va a recibir como parametro el AccessViewModel
+        {
+            Reply oReply = new Reply();
+            oReply.result = 0;
+            try
+            {
+                using (Cursomvc_apiEntities db = new Cursomvc_apiEntities())
+                {
+                    var lst = from d in db.user
+                              where d.email == model.email && d.password == model.password && d.idEstatus == 1
+                              select d;
+
+                     if(lst.Count() > 0)
+                     {
+                        oReply.result = 1;
+                        oReply.message = "Token asignado: ";
+                        // Creo token
+                        oReply.data = Guid.NewGuid().ToString(); // Genera identificador único, para ser implementado como token
+
+                        // Para guardar token en la db
+                        user oUser = lst.FirstOrDefault();
+                        oUser.token = (string)oReply.data;
+                        // Para editar valor en db
+                        db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                     }
+                     else
+                     {
+                        oReply.message = "Datos incorrectos";
+                     }
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                oReply.message = "Hubo un error" + ex;
+            }
+            return oReply;
+        }
+
     }
 }
---
+++ Models/WS/AccessViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_API_FUNDAMENTALS.Models.WS
+{
+    // 6. Agrego clase AccessViewModel
+    public class AccessViewModel
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
