48090d2e33398ec1995492c7b7bb52cc641650cf
Ed. Gi <31946346+dwaxgio@users.noreply.github.com>
2020-10-13 13:05:45 -0500

    Se agregó AnimalViewModel con atributos name y patas. Se agregó método Add de tipo Reply en AnimalController, con parametro de entrada el modelo AnimalViewModel que a su vez hereda de SecurityViewModel para validación de token

--- Controllers/AnimalController.cs
+++ Controllers/AnimalController.cs
@@ -49,5 +49,50 @@ namespace MVC_API_FUNDAMENTALS.Controllers
 
             return oReply;
         }
+
+        //11. Agreo metodo para add
+        [HttpPost]
+        public Reply Add([FromBody] AnimalViewModel model)
+        {
+            Reply oReply = new Reply();
+            oReply.result = 0;
+
+            // 11.2 se agrega try catch
+            try
+            {
+                using(Cursomvc_apiEntities DB = new Cursomvc_apiEntities())
+                {
+                    // Se crea objeto de la entidad de la db para agregar a la db
+                    animal oAnimal = new animal();
+                    oAnimal.idState = 1;
+                    oAnimal.name = model.name;
+                    oAnimal.patas = model.patas;
+
+                    // Se agrega objeto a db
+                    DB.animal.Add(oAnimal);
+                    DB.SaveChanges();
+
+                    // Para respuesta a petición api
+                    List<ListAnimalsViewModel> lst = (from d in DB.animal
+                                                      where d.idState == 1
+                                                      select new ListAnimalsViewModel // genera nuevo listado de animales de la tabla en la db
+                                                      {
+                                                          name = d.name, // del atributo del AnimalViewModel, asignar lo que esta en la tabla de la db en campo name
+                                                          patas = d.patas
+                                                      }).ToList();
+
+                    oReply.result = 1;
+                    oReply.message = "Dato agregado: ";
+                    oReply.data = lst; // Para que el cliente no tenga que solicitar nuevamente la data, se remite listado de entidad
+                }
+
+            }
+            catch (Exception ex)
+            {
+                oReply.message = "Error, intente nuevamente..." + ex;                
+            }
+            return oReply;
+        }
+
     }
 }
---
+++ Models/WS/AnimalViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_API_FUNDAMENTALS.Models.WS
+{
+    // 10. Agrego AnimalViewModel para manejar los datos referentes a las operaciones crud del animal
+    public class AnimalViewModel : SecurityViewModel // Clase hereda de SecurityViewModel, que contiene token
+    {
+        public int id { get; set; } // Se utiliza para el Delete y el Update
+        public string name { get; set; }
+        public int patas { get; set; }
+    }
+}
